0b4fc1314f8488f32b219a2ee178560174698e1d
thomasfrl <feral.thomas@gmail.com>
2019-05-14 01:43:49 +0200

    home root made

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "fights#index"
+  root to: "static#home"
   resources :fights, except: [:update, :edit, :destroy]
   resources :fighters
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
